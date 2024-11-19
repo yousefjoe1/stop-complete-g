2e65d312c62d9cc0489b83333e3b6b43abd2cf46
yousefjoe1 <yousefmahmoud150@gmail.com>
2024-11-19 22:18:15 +0200

    add pwa pckgs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+let myconfig :any = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  manifest: {
+    name: 'Stop Complete',
+    short_name: 'I Am Finished',
+    description: 'Shop app - categories - cloths - shoess - shirts',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  }
+}
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA(myconfig)],
+  define: {
+    'process.env.BUILD_VERSION': JSON.stringify(process.env.BUILD_VERSION || Date.now())
+  }
 })
+
